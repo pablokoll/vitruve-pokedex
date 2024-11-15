fa23c6238672a33b1b407fe34741690aa1710769
ideapad-w11-wsl <pablokollm@gmail.com>
2024-11-15 14:33:35 +0100

    setup base

--- ui/panda.config.ts
+++ ui/panda.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from '@pandacss/dev';
+import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
-    preflight: true,
-    include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
-    exclude: [],
-    outdir: 'styled-system',
+	preflight: true,
+	include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+	exclude: [],
+	outdir: "styled-system",
+	presets: ["@pandacss/preset-base", "@pandacss/preset-panda"],
+	prefix: "panda",
 });
---
+++ ui/src/styles/styles.ts
@@ -0,0 +1,20 @@
+import { css } from "../../styled-system/css";
+
+export const buttonStyle = css({
+  color: "white",
+  py: 2,
+  px: 4,
+  rounded: "md",
+  fontSize: "md",
+  width: '100%',
+  background: 'none'
+});
+
+export const containerStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "start",
+  padding: "10",
+  height: "100%",
+});
